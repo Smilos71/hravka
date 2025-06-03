8a169910ebdd5855a18f1275d6e857b193aefd3f
Smilos71 <147918915+Smilos71@users.noreply.github.com>
2025-06-03 16:12:25 +0200

    a

--- data.js
+++ data.js
@@ -14,4 +14,18 @@ const games = [
         description: "Tower Defense game by NINJA KIWI",
         url: "https://cdn2.addictinggames.com/addictinggames-content/ag-assets/content-items/html5-games/bloonstowerdefense4/index.html"
     },
+        {
+        id: "fitelol",
+        name: "Fite.lol",
+        icon: "https://www.addictinggames.com/_next/image?url=https%3A%2F%2Fprod.addictinggames.com%2Fsites%2Fdefault%2Ffiles%2Ffitelol_promo_720_468%2520%25281%2529.png&w=1920&q=100",
+        description: "A fun online fighting game",
+        url: "https://www.addictinggames.com/embed/html5-games/10000693"
+    },
+            {
+        id: "gtasim",
+        name: "GTA Simulator",
+        icon: "https://www.onlinegames.io/media/posts/416/responsive/GTA-Simulator-lg.jpg",
+        description: "Play GTA Simulator - A fun online sandbox game!",
+        url: "https://www.onlinegames.io/games/2023/unity2/gta-simulator/index.html"
+    },
 ];
