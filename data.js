02a57e128b315bbb2458ae9f0d4ff765f61bd345
Smilos71 <147918915+Smilos71@users.noreply.github.com>
2025-06-03 14:59:27 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+const games = [
+    {
+        id: "subway",
+        name: "Subway Surfers",
+        icon: "https://cdnb.artstation.com/p/assets/images/images/040/799/969/large/celine-j-icon-1024-ios.jpg?1629908125",
+        description: "Run as fast as you can while dodging trains, collecting coins, and unlocking new characters! Subway Surfers is an endless runner that challenges your reflexes and skills.",
+        url: "https://www.jopi.com/embed.php?game=subway-surfers"
+    },
+    // Další hry můžeš přidat sem ve stejném formátu
+];
