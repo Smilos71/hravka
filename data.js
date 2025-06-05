6c2ddd9fd2c4c91a97ff02c902474cf1a6d0e1b6
Smilos71 <147918915+Smilos71@users.noreply.github.com>
2025-06-05 14:56:08 +0200

    a

--- data.js
+++ data.js
@@ -15,11 +15,11 @@ const games = [
         url: "https://cdn2.addictinggames.com/addictinggames-content/ag-assets/content-items/html5-games/bloonstowerdefense4/index.html"
     },
         {
-        id: "fitelol",
-        name: "Fite.lol",
+        id: "minecraft",
+        name: "Minecraft Java",
         icon: "https://www.addictinggames.com/_next/image?url=https%3A%2F%2Fprod.addictinggames.com%2Fsites%2Fdefault%2Ffiles%2Ffitelol_promo_720_468%2520%25281%2529.png&w=1920&q=100",
         description: "A fun online fighting game",
-        url: "https://www.addictinggames.com/embed/html5-games/10000693"
+        url: "https://eaglercraft.com/mc/1.8.8/"
     },
             {
         id: "gtasim",
